594ac6efb9d66db28c5f0ce91a2ca81206c195dd
zoltan_almodo <zoltanalmodo@gmail.com>
2020-11-16 03:03:08 +0100

    start component created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Disco } from './components/Disco';
 import { Black } from './components/Black';
-import Navigation from './components/Navigation';
+import { Start } from './components/Start';
 import { White } from './components/White';
 import { GlobalProvider } from './context/GlobalState';
 import { Color } from './components/Color';
@@ -15,7 +15,7 @@ function App() {
     <GlobalProvider>
       
         <Router>
-          <Route path='/' exact component={Navigation} />
+          <Route path='/' exact component={Start} />
           <Route path='/disco' component={Disco} />
           <Route path='/black' component={Black} />
           <Route path='/white' component={White} />
---
+++ src/components/Start.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navigation from './Navigation';
+
+
+
+export const Start = () => {
+
+  return (
+    
+    <div className='body_white'>
+       
+      <Navigation />
+      <h1>Click a button!</h1>
+      <h2>Click and drag or swipe the drawings to play!</h2>
+      
+    </div>
+  )
+}
