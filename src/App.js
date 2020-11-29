fe9ae26e5cb8b882493e2ba86cf54c13d00ffbb8
zoltan_almodo <zoltanalmodo@gmail.com>
2020-11-29 06:44:42 +0100

    orientation css added to iPhone 6, 6s, 7 and 8

--- src/App.js
+++ src/App.js
@@ -4,33 +4,32 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Disco } from './components/Disco';
 import { Black } from './components/Black';
 import { Start } from './components/Start';
-import { Orient } from './components/Orient';
+
 import { White } from './components/White';
 import { GlobalProvider } from './context/GlobalState';
 import { Color } from './components/Color';
 
-import DeviceOrientation, { Orientation } from 'react-screen-orientation'
+// import DeviceOrientation, { Orientation } from 'react-screen-orientation'
 
 function App() {
   return (
     <GlobalProvider>
 
-      <DeviceOrientation lockOrientation={'portrait'}>
-        <Orientation orientation='portrait' alwaysRender={false}>
+      {/* <DeviceOrientation lockOrientation={'portrait'}>
+        
+        <Orientation orientation='portrait' alwaysRender={false}> */}
 
           <Router>
-            <Route path='/' exact component={Start} />
-            <Route path='/disco' component={Disco} />
-            <Route path='/black' component={Black} />
-            <Route path='/white' component={White} />
-            <Route path='/color' component={Color} />
+              <Route path='/' exact component={Start} />
+              <Route path='/disco' component={Disco} />
+              <Route path='/black' component={Black} />
+              <Route path='/white' component={White} />
+              <Route path='/color' component={Color} />
           </Router>
 
-        </Orientation>
-        <Orientation orientation='landscape'>
-          <Orient />
-        </Orientation>
-      </DeviceOrientation>
+        {/* </Orientation> */}
+        
+      {/* </DeviceOrientation> */}
 
     </GlobalProvider>
 
