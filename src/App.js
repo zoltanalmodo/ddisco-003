59e224eebf1d4aa43cce15903892ddf8da811b96
zoltan_almodo <zoltanalmodo@gmail.com>
2020-11-29 23:13:15 +0100

    switch to react-native-app

--- src/App.js
+++ src/App.js
@@ -9,27 +9,19 @@ import { White } from './components/White';
 import { GlobalProvider } from './context/GlobalState';
 import { Color } from './components/Color';
 
-// import DeviceOrientation, { Orientation } from 'react-screen-orientation'
+
 
 function App() {
   return (
     <GlobalProvider>
 
-      {/* <DeviceOrientation lockOrientation={'portrait'}>
-        
-        <Orientation orientation='portrait' alwaysRender={false}> */}
-
-          <Router>
-              <Route path='/' exact component={Start} />
-              <Route path='/disco' component={Disco} />
-              <Route path='/black' component={Black} />
-              <Route path='/white' component={White} />
-              <Route path='/color' component={Color} />
-          </Router>
-
-        {/* </Orientation> */}
-        
-      {/* </DeviceOrientation> */}
+      <Router>
+          <Route path='/' exact component={Start} />
+          <Route path='/disco' component={Disco} />
+          <Route path='/black' component={Black} />
+          <Route path='/white' component={White} />
+          <Route path='/color' component={Color} />
+      </Router>
 
     </GlobalProvider>
 
