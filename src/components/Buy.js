c7115620c1f2b12d81a9fd161c79b64a4ec62e00
zoltan almodo <zoltanalmodo@gmail.com>
2024-11-19 23:27:50 +0100

    CSS cleanup buy page = better

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -106,22 +106,27 @@ export const Buy = () => {
 
       <div className="carousel_white_buy">
 
-        <div style={{
-          filter: (globalState.isPastel === 'true' ? 'brightness(105%) saturate(70%)' : 'brightness(100%) saturate(100%)'),
-        }}>
+        <div className="frame-overlay-dark-grey-buy">
 
-          <div className="frame-overlay-dark-grey-buy"></div>
-
-          <div className="single-image-display">
-            {selectedImage_001}
-          </div>
-          <div className="single-image-display">
-            {selectedImage_002}
-          </div>
-          <div className="single-image-display">
-            {selectedImage_003}
-          </div>
+          <div style={{
+            filter: (globalState.isPastel === 'true' ? 'brightness(105%) saturate(70%)' : 'brightness(100%) saturate(100%)'),
             
+          }}>
+
+            <div className="single_image_display_container">
+              <div className="single_image_display">
+                {selectedImage_001}
+              </div>
+              <div className="single_image_display">
+                {selectedImage_002}
+              </div>
+              <div className="single_image_display">
+                {selectedImage_003}
+              </div>
+            </div>
+
+          </div>
+
         </div>
 
         <div class="sizes_container">
