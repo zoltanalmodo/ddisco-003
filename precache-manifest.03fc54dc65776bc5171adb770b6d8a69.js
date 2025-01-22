a573f401a61da6f2b48be326f74e7ca4afde3c15
zoltan almodo <zoltanalmodo@gmail.com>
2025-01-22 23:55:02 +0100

    Deploy build to GitHub Pages

---
+++ precache-manifest.03fc54dc65776bc5171adb770b6d8a69.js
@@ -0,0 +1,466 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8698784a091885db74c81d4fd93cf45b",
+    "url": "/ddisco-003/index.html"
+  },
+  {
+    "revision": "2ff686d2e3cb0ed0d4e5",
+    "url": "/ddisco-003/static/css/2.55d5623b.chunk.css"
+  },
+  {
+    "revision": "04cce6c46b3cfc051e56",
+    "url": "/ddisco-003/static/css/main.dd6779a7.chunk.css"
+  },
+  {
+    "revision": "2ff686d2e3cb0ed0d4e5",
+    "url": "/ddisco-003/static/js/2.5bf07c49.chunk.js"
+  },
+  {
+    "revision": "2aa2de25c492e48ff1984943803c47f7",
+    "url": "/ddisco-003/static/js/2.5bf07c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3701ee1e700085386725",
+    "url": "/ddisco-003/static/js/3.6d294f16.chunk.js"
+  },
+  {
+    "revision": "04cce6c46b3cfc051e56",
+    "url": "/ddisco-003/static/js/main.80710d26.chunk.js"
+  },
+  {
+    "revision": "40c4d6cc35ab223edc8a",
+    "url": "/ddisco-003/static/js/runtime-main.33ee1868.js"
+  },
+  {
+    "revision": "ed9d9c7f7705e3464571a885a214b53c",
+    "url": "/ddisco-003/static/media/ddisco_b_001_1.ed9d9c7f.jpg"
+  },
+  {
+    "revision": "8a65b20715e9187069cc9c7af35e68e5",
+    "url": "/ddisco-003/static/media/ddisco_b_001_2.8a65b207.jpg"
+  },
+  {
+    "revision": "814cd6f3466cbcb90bc455c331e2ea79",
+    "url": "/ddisco-003/static/media/ddisco_b_001_3.814cd6f3.jpg"
+  },
+  {
+    "revision": "3f3df3b699973772ee46f68e4ae49a0b",
+    "url": "/ddisco-003/static/media/ddisco_b_002_1.3f3df3b6.jpg"
+  },
+  {
+    "revision": "ec18b2200244d6bab908b06639162f54",
+    "url": "/ddisco-003/static/media/ddisco_b_002_2.ec18b220.jpg"
+  },
+  {
+    "revision": "7f825a305382a22f67feff1ea9d731c7",
+    "url": "/ddisco-003/static/media/ddisco_b_002_3.7f825a30.jpg"
+  },
+  {
+    "revision": "563c97ea996a5997b27867603342da8b",
+    "url": "/ddisco-003/static/media/ddisco_b_003_1.563c97ea.jpg"
+  },
+  {
+    "revision": "a6ce209c416df2a034dd3866e8ddafb3",
+    "url": "/ddisco-003/static/media/ddisco_b_003_2.a6ce209c.jpg"
+  },
+  {
+    "revision": "8e6dc9fb3ed32308bab6133ed76e1217",
+    "url": "/ddisco-003/static/media/ddisco_b_003_3.8e6dc9fb.jpg"
+  },
+  {
+    "revision": "29c7c0de4da048e4eca23cc2895f9d65",
+    "url": "/ddisco-003/static/media/ddisco_b_004_1.29c7c0de.jpg"
+  },
+  {
+    "revision": "65274d6d01b95889816b8cf8886f7a57",
+    "url": "/ddisco-003/static/media/ddisco_b_004_2.65274d6d.jpg"
+  },
+  {
+    "revision": "2492285c69504c85d8c6f565adfce843",
+    "url": "/ddisco-003/static/media/ddisco_b_004_3.2492285c.jpg"
+  },
+  {
+    "revision": "980372918e0ccbd10af188ee0fdfea7d",
+    "url": "/ddisco-003/static/media/ddisco_b_005_1.98037291.jpg"
+  },
+  {
+    "revision": "7482febaf7bfc0f6842496b311b71967",
+    "url": "/ddisco-003/static/media/ddisco_b_005_2.7482feba.jpg"
+  },
+  {
+    "revision": "cb716f31310171f7171e7ea49fba5c0b",
+    "url": "/ddisco-003/static/media/ddisco_b_005_3.cb716f31.jpg"
+  },
+  {
+    "revision": "80c5adac72ad5cbb041ca85d9b75d260",
+    "url": "/ddisco-003/static/media/ddisco_b_006_1.80c5adac.jpg"
+  },
+  {
+    "revision": "6c64e0f489c4cb81261a7c75fb49a0b6",
+    "url": "/ddisco-003/static/media/ddisco_b_006_2.6c64e0f4.jpg"
+  },
+  {
+    "revision": "9cca4081b398c59a531085130bd5393b",
+    "url": "/ddisco-003/static/media/ddisco_b_006_3.9cca4081.jpg"
+  },
+  {
+    "revision": "50be8de03d966f3811b07f277d59024a",
+    "url": "/ddisco-003/static/media/ddisco_b_007_1.50be8de0.jpg"
+  },
+  {
+    "revision": "60dd0b2d27b8b603b0ec22c6c3b97e09",
+    "url": "/ddisco-003/static/media/ddisco_b_007_2.60dd0b2d.jpg"
+  },
+  {
+    "revision": "7e90183d61deef61c88067eb1d5ec885",
+    "url": "/ddisco-003/static/media/ddisco_b_007_3.7e90183d.jpg"
+  },
+  {
+    "revision": "e7263e4fd7f59aea6050c44817cb2cfc",
+    "url": "/ddisco-003/static/media/ddisco_b_008_1.e7263e4f.jpg"
+  },
+  {
+    "revision": "7dc7614bf0d550215a9bd4e8711f01fa",
+    "url": "/ddisco-003/static/media/ddisco_b_008_2.7dc7614b.jpg"
+  },
+  {
+    "revision": "9715e55b4b9556da116300a4b9fae6ff",
+    "url": "/ddisco-003/static/media/ddisco_b_008_3.9715e55b.jpg"
+  },
+  {
+    "revision": "d09c67e267f69183c231af3c04e89f8a",
+    "url": "/ddisco-003/static/media/ddisco_b_009_1.d09c67e2.jpg"
+  },
+  {
+    "revision": "b99f62e11f36662ed0816219676f9888",
+    "url": "/ddisco-003/static/media/ddisco_b_009_2.b99f62e1.jpg"
+  },
+  {
+    "revision": "1cbd8ea83e74cb719159138db0d64c73",
+    "url": "/ddisco-003/static/media/ddisco_b_009_3.1cbd8ea8.jpg"
+  },
+  {
+    "revision": "136b3baeaba479b2610bb6bd602fbe6c",
+    "url": "/ddisco-003/static/media/ddisco_b_010_1.136b3bae.jpg"
+  },
+  {
+    "revision": "76dbabcc96b1eb05fe0dc732fe3906b8",
+    "url": "/ddisco-003/static/media/ddisco_b_010_2.76dbabcc.jpg"
+  },
+  {
+    "revision": "3049d74ac4cdaf9c4eea5683bb4b5533",
+    "url": "/ddisco-003/static/media/ddisco_b_010_3.3049d74a.jpg"
+  },
+  {
+    "revision": "ab5200c1a7baa8088fd38433e6480061",
+    "url": "/ddisco-003/static/media/ddisco_b_011_1.ab5200c1.jpg"
+  },
+  {
+    "revision": "2d3db7bb869c7e08fce43ba26d36b3c8",
+    "url": "/ddisco-003/static/media/ddisco_b_011_2.2d3db7bb.jpg"
+  },
+  {
+    "revision": "7b679e1b3b29efde5975dc061940f34a",
+    "url": "/ddisco-003/static/media/ddisco_b_011_3.7b679e1b.jpg"
+  },
+  {
+    "revision": "66d57c6304416cef8e9f5e0b54f1d2b3",
+    "url": "/ddisco-003/static/media/ddisco_b_012_1.66d57c63.jpg"
+  },
+  {
+    "revision": "14f9e5d87e21b88f43d95d7759111ca2",
+    "url": "/ddisco-003/static/media/ddisco_b_012_2.14f9e5d8.jpg"
+  },
+  {
+    "revision": "f226df6d5330f953f871e1490dcb98ce",
+    "url": "/ddisco-003/static/media/ddisco_b_012_3.f226df6d.jpg"
+  },
+  {
+    "revision": "c19c37736f2b0410a4506b4ad210d124",
+    "url": "/ddisco-003/static/media/ddisco_c_001_1.c19c3773.jpg"
+  },
+  {
+    "revision": "cc6790d0536cf0b07c38c8a19239b5b4",
+    "url": "/ddisco-003/static/media/ddisco_c_001_2.cc6790d0.jpg"
+  },
+  {
+    "revision": "5dda24407a768be3047057a48ac213c3",
+    "url": "/ddisco-003/static/media/ddisco_c_001_3.5dda2440.jpg"
+  },
+  {
+    "revision": "82259a54a91a3d670acdb6b29070bde7",
+    "url": "/ddisco-003/static/media/ddisco_c_002_1.82259a54.jpg"
+  },
+  {
+    "revision": "a86da2baf97c0f44b2df786e9e6053bc",
+    "url": "/ddisco-003/static/media/ddisco_c_002_2.a86da2ba.jpg"
+  },
+  {
+    "revision": "bbc37d44d192035834086321645bb21d",
+    "url": "/ddisco-003/static/media/ddisco_c_002_3.bbc37d44.jpg"
+  },
+  {
+    "revision": "76d0345d04fd801b3d10713a26237aad",
+    "url": "/ddisco-003/static/media/ddisco_c_003_1.76d0345d.jpg"
+  },
+  {
+    "revision": "28963798ef465dd468fc3a9d4960f8c2",
+    "url": "/ddisco-003/static/media/ddisco_c_003_2.28963798.jpg"
+  },
+  {
+    "revision": "7a7c153cdf710a0465eaebb0cd14bc6e",
+    "url": "/ddisco-003/static/media/ddisco_c_003_3.7a7c153c.jpg"
+  },
+  {
+    "revision": "25dfca889da0fce989f58d41f7133afb",
+    "url": "/ddisco-003/static/media/ddisco_c_004_1.25dfca88.jpg"
+  },
+  {
+    "revision": "2f1f2075d19cd4a6cdff277d591415fe",
+    "url": "/ddisco-003/static/media/ddisco_c_004_2.2f1f2075.jpg"
+  },
+  {
+    "revision": "796089d121cc184251659747d64a77c0",
+    "url": "/ddisco-003/static/media/ddisco_c_004_3.796089d1.jpg"
+  },
+  {
+    "revision": "9e28896ec617c519f8e66c969b76d15b",
+    "url": "/ddisco-003/static/media/ddisco_c_005_1.9e28896e.jpg"
+  },
+  {
+    "revision": "9e6ac476e6e47b939058c6e6f49ddd44",
+    "url": "/ddisco-003/static/media/ddisco_c_005_2.9e6ac476.jpg"
+  },
+  {
+    "revision": "7860707ad35a32ddab46c828ad392715",
+    "url": "/ddisco-003/static/media/ddisco_c_005_3.7860707a.jpg"
+  },
+  {
+    "revision": "99b9b679f922882e56a239d4ea18a23d",
+    "url": "/ddisco-003/static/media/ddisco_c_006_1.99b9b679.jpg"
+  },
+  {
+    "revision": "b5295ece675c48bdb67bd16fab29ceff",
+    "url": "/ddisco-003/static/media/ddisco_c_006_2.b5295ece.jpg"
+  },
+  {
+    "revision": "26d6f7d8e2220de2a9faee0e8725a1bd",
+    "url": "/ddisco-003/static/media/ddisco_c_006_3.26d6f7d8.jpg"
+  },
+  {
+    "revision": "db69354916e0993ddd6695da9718100d",
+    "url": "/ddisco-003/static/media/ddisco_c_007_1.db693549.jpg"
+  },
+  {
+    "revision": "ba2d946b0e006a2ab8de889a859fd635",
+    "url": "/ddisco-003/static/media/ddisco_c_007_2.ba2d946b.jpg"
+  },
+  {
+    "revision": "afc6ba2ce2ec59c034403297315aeb95",
+    "url": "/ddisco-003/static/media/ddisco_c_007_3.afc6ba2c.jpg"
+  },
+  {
+    "revision": "018eced204856b405c07a5589799578b",
+    "url": "/ddisco-003/static/media/ddisco_c_008_1.018eced2.jpg"
+  },
+  {
+    "revision": "2aadac894d79d0346ca15db98ba124c0",
+    "url": "/ddisco-003/static/media/ddisco_c_008_2.2aadac89.jpg"
+  },
+  {
+    "revision": "9bae24d94660732da95e23658232149e",
+    "url": "/ddisco-003/static/media/ddisco_c_008_3.9bae24d9.jpg"
+  },
+  {
+    "revision": "4076913b37d2753d83b6cb9a564dcb78",
+    "url": "/ddisco-003/static/media/ddisco_c_009_1.4076913b.jpg"
+  },
+  {
+    "revision": "3efb537a0a89a29564b144d130257e20",
+    "url": "/ddisco-003/static/media/ddisco_c_009_2.3efb537a.jpg"
+  },
+  {
+    "revision": "b3f57ae18cfcb5f3e4e9956962a8eedb",
+    "url": "/ddisco-003/static/media/ddisco_c_009_3.b3f57ae1.jpg"
+  },
+  {
+    "revision": "df38e35c4b6de8b099d27bf846982706",
+    "url": "/ddisco-003/static/media/ddisco_c_010_1.df38e35c.jpg"
+  },
+  {
+    "revision": "f645586201f9a06a67c088e8f994aa37",
+    "url": "/ddisco-003/static/media/ddisco_c_010_2.f6455862.jpg"
+  },
+  {
+    "revision": "1ea2c3a1267f1d911995dd6a42de8326",
+    "url": "/ddisco-003/static/media/ddisco_c_010_3.1ea2c3a1.jpg"
+  },
+  {
+    "revision": "13fa11012833091b4a731fccf3bd4f3f",
+    "url": "/ddisco-003/static/media/ddisco_c_011_1.13fa1101.jpg"
+  },
+  {
+    "revision": "7f7b89f7dcc87557ce72da78f31df98a",
+    "url": "/ddisco-003/static/media/ddisco_c_011_2.7f7b89f7.jpg"
+  },
+  {
+    "revision": "7c6537b843dfae46dca1e1bc4a257b3f",
+    "url": "/ddisco-003/static/media/ddisco_c_011_3.7c6537b8.jpg"
+  },
+  {
+    "revision": "80248b4ac5a8f4e31a460ca8c65f601d",
+    "url": "/ddisco-003/static/media/ddisco_c_012_1.80248b4a.jpg"
+  },
+  {
+    "revision": "2deb144a379e6f6b3d4cef6016bab706",
+    "url": "/ddisco-003/static/media/ddisco_c_012_2.2deb144a.jpg"
+  },
+  {
+    "revision": "0667a2d5b02fb73f7b8901c2f286ffd0",
+    "url": "/ddisco-003/static/media/ddisco_c_012_3.0667a2d5.jpg"
+  },
+  {
+    "revision": "4e3e9fb1f8dcf6c6ae103ccd09c8943b",
+    "url": "/ddisco-003/static/media/ddisco_w_001_1.4e3e9fb1.jpg"
+  },
+  {
+    "revision": "98904b5001aee405ed07c7edfceb0dc6",
+    "url": "/ddisco-003/static/media/ddisco_w_001_2.98904b50.jpg"
+  },
+  {
+    "revision": "d59487ec1163d58318ddf199f881d335",
+    "url": "/ddisco-003/static/media/ddisco_w_001_3.d59487ec.jpg"
+  },
+  {
+    "revision": "a0aa372e8fa50f911a45ecbdfb7d6185",
+    "url": "/ddisco-003/static/media/ddisco_w_002_1.a0aa372e.jpg"
+  },
+  {
+    "revision": "7866901937c1dadc3bf003c1a09ddeae",
+    "url": "/ddisco-003/static/media/ddisco_w_002_2.78669019.jpg"
+  },
+  {
+    "revision": "63febf42335eebb7fcd3dce16dcef6fd",
+    "url": "/ddisco-003/static/media/ddisco_w_002_3.63febf42.jpg"
+  },
+  {
+    "revision": "bb0860918901282e9c70303d8ce17af7",
+    "url": "/ddisco-003/static/media/ddisco_w_003_1.bb086091.jpg"
+  },
+  {
+    "revision": "c220a303752cf31df022f0181cc0240f",
+    "url": "/ddisco-003/static/media/ddisco_w_003_2.c220a303.jpg"
+  },
+  {
+    "revision": "48ec009fe99e9259315fa78d0245101e",
+    "url": "/ddisco-003/static/media/ddisco_w_003_3.48ec009f.jpg"
+  },
+  {
+    "revision": "db40a79925989fa45e4fe744ee47310e",
+    "url": "/ddisco-003/static/media/ddisco_w_004_1.db40a799.jpg"
+  },
+  {
+    "revision": "51d1e06dbff74b91ecad67ba0cde888d",
+    "url": "/ddisco-003/static/media/ddisco_w_004_2.51d1e06d.jpg"
+  },
+  {
+    "revision": "6c966a7481716abf40101cfc97beda43",
+    "url": "/ddisco-003/static/media/ddisco_w_004_3.6c966a74.jpg"
+  },
+  {
+    "revision": "3ab5038e9939e2ccc6740ebd9148464b",
+    "url": "/ddisco-003/static/media/ddisco_w_005_1.3ab5038e.jpg"
+  },
+  {
+    "revision": "a290d5265de82e878c7780f47eb5ab74",
+    "url": "/ddisco-003/static/media/ddisco_w_005_2.a290d526.jpg"
+  },
+  {
+    "revision": "83f894da0cbcd8606286c22f8ac5f911",
+    "url": "/ddisco-003/static/media/ddisco_w_005_3.83f894da.jpg"
+  },
+  {
+    "revision": "632d5f24ed979fd1fcf83e469d9ee844",
+    "url": "/ddisco-003/static/media/ddisco_w_006_1.632d5f24.jpg"
+  },
+  {
+    "revision": "a074e8947bd4a83b36432b331f74e337",
+    "url": "/ddisco-003/static/media/ddisco_w_006_2.a074e894.jpg"
+  },
+  {
+    "revision": "fb34ff072328bca21b34688d8a9d1717",
+    "url": "/ddisco-003/static/media/ddisco_w_006_3.fb34ff07.jpg"
+  },
+  {
+    "revision": "9d9d504f521c330a8cab8456229aed39",
+    "url": "/ddisco-003/static/media/ddisco_w_007_1.9d9d504f.jpg"
+  },
+  {
+    "revision": "6313a3b7606c1e60712f551289791445",
+    "url": "/ddisco-003/static/media/ddisco_w_007_2.6313a3b7.jpg"
+  },
+  {
+    "revision": "515a176fde2d176068458b10a6d0e4be",
+    "url": "/ddisco-003/static/media/ddisco_w_007_3.515a176f.jpg"
+  },
+  {
+    "revision": "7a1ee561ad973ef13f866fd225fdfae7",
+    "url": "/ddisco-003/static/media/ddisco_w_008_1.7a1ee561.jpg"
+  },
+  {
+    "revision": "abe7b88f53d42917dedb32594f4abcd9",
+    "url": "/ddisco-003/static/media/ddisco_w_008_2.abe7b88f.jpg"
+  },
+  {
+    "revision": "8efb861356ea2a7b65584b390e1f3d95",
+    "url": "/ddisco-003/static/media/ddisco_w_008_3.8efb8613.jpg"
+  },
+  {
+    "revision": "456a52e33142c996aab6e30d3caa821b",
+    "url": "/ddisco-003/static/media/ddisco_w_009_1.456a52e3.jpg"
+  },
+  {
+    "revision": "f1adff56d4f53be6e246fd407ca3f627",
+    "url": "/ddisco-003/static/media/ddisco_w_009_2.f1adff56.jpg"
+  },
+  {
+    "revision": "ae01882260d305e8dbd811c10c72749a",
+    "url": "/ddisco-003/static/media/ddisco_w_009_3.ae018822.jpg"
+  },
+  {
+    "revision": "c23c3a03c68ae21563c60977092d1541",
+    "url": "/ddisco-003/static/media/ddisco_w_010_1.c23c3a03.jpg"
+  },
+  {
+    "revision": "6f0c6f5be78c863a04866076895c70d7",
+    "url": "/ddisco-003/static/media/ddisco_w_010_2.6f0c6f5b.jpg"
+  },
+  {
+    "revision": "6d8fc555538c57fe88afc0ec22471712",
+    "url": "/ddisco-003/static/media/ddisco_w_010_3.6d8fc555.jpg"
+  },
+  {
+    "revision": "35a17892874321d253b871d2b6779a06",
+    "url": "/ddisco-003/static/media/ddisco_w_011_1.35a17892.jpg"
+  },
+  {
+    "revision": "52497b971bcdde18555c1d8e3e04f011",
+    "url": "/ddisco-003/static/media/ddisco_w_011_2.52497b97.jpg"
+  },
+  {
+    "revision": "81b03c3f384b7a66e016094c6edafadc",
+    "url": "/ddisco-003/static/media/ddisco_w_011_3.81b03c3f.jpg"
+  },
+  {
+    "revision": "0d4dd46709223ec2ff3503c31a933cd9",
+    "url": "/ddisco-003/static/media/ddisco_w_012_1.0d4dd467.jpg"
+  },
+  {
+    "revision": "a87cfbad517ccda976117eb318ddc7ef",
+    "url": "/ddisco-003/static/media/ddisco_w_012_2.a87cfbad.jpg"
+  },
+  {
+    "revision": "caf8d6d3fc689633750b1ae2a10c91fa",
+    "url": "/ddisco-003/static/media/ddisco_w_012_3.caf8d6d3.jpg"
+  }
+]);
