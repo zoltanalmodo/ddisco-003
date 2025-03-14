4f99801a1203ffc475e72dc6114c28e331ae48f7
zoltan almodo <zoltanalmodo@gmail.com>
2025-03-14 23:01:14 +0100

    ters and conditions populate further

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -5,18 +5,40 @@ export const Terms = () => {
     <div className='body_white_buy'>
       <div className="terms_and_conditions">
         <div>
-        <h1>Terms and Conditions for Newsletter Subscription</h1>
+        <h2>Terms and Conditions for Newsletter Subscription</h2>
           <h1>1. Introduction</h1>
             <p>These Terms and Conditions govern your subscription to our newsletter service. 
                 By subscribing to our newsletter, you accept these Terms and Conditions in full. 
                 If you disagree with these Terms and Conditions or any part of them, 
                 you should not subscribe to our newsletter.</p>
-        
-          <h1>2. Definitions</h1>
+        <h1>2. Definitions</h1>
             <p>These Terms and Conditions govern your subscription to our newsletter service. 
                 By subscribing to our newsletter, you accept these Terms and Conditions in full. 
                 If you disagree with these Terms and Conditions or any part of them, 
                 you should not subscribe to our newsletter.</p>
+        <h1>4. Personal Data Collection and Use</h1>
+            <p>3.1. By subscribing to our newsletter, you agree to receive periodic communications from us. 
+            3.2. To subscribe to our newsletter, you must provide a valid email address and any other 
+            information requested in the subscription form.
+            3.3. We reserve the right to refuse or cancel your subscription at our discretion.</p>
+        <h1>3. Subscription</h1>
+            <p>4.1. We collect and process your personal data in accordance with applicable data protection laws.
+4.2. Personal data we collect may include, but is not limited to:
+
+Your name
+Your email address
+Your location
+Subscription preferences
+
+4.3. We use your personal data for the following purposes:
+
+Sending you our newsletter
+Providing you with information about our products, services, offers, and announcements
+Improving our newsletter content
+Analyzing subscription statistics
+
+4.4. We will not share, sell, rent, or trade your personal information with third parties for their commercial purposes without your explicit consent.
+4.5. We implement appropriate technical and organizational measures to protect your personal data against unauthorized or unlawful processing and against accidental loss, destruction, or damage.</p>
         </div>
       </div>
     </div>  
