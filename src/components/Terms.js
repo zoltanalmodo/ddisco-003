d05d44432fefa71da50d12f2dc3b5bdf57d74224
zoltan almodo <zoltanalmodo@gmail.com>
2025-03-14 13:42:27 +0100

    propagate terms page with content

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -3,10 +3,20 @@ import React from 'react';
 export const Terms = () => {
   return (
     <div className='body_white_buy'>
-      <div className="orders_list">
+      <div className="terms_and_conditions">
         <div>
-          <h1>Terms and Conditions:</h1>
-          <p>paragraph</p>
+        <h1>Terms and Conditions for Newsletter Subscription</h1>
+          <h1>1. Introduction</h1>
+            <p>These Terms and Conditions govern your subscription to our newsletter service. 
+                By subscribing to our newsletter, you accept these Terms and Conditions in full. 
+                If you disagree with these Terms and Conditions or any part of them, 
+                you should not subscribe to our newsletter.</p>
+        
+          <h1>2. Definitions</h1>
+            <p>These Terms and Conditions govern your subscription to our newsletter service. 
+                By subscribing to our newsletter, you accept these Terms and Conditions in full. 
+                If you disagree with these Terms and Conditions or any part of them, 
+                you should not subscribe to our newsletter.</p>
         </div>
       </div>
     </div>  
